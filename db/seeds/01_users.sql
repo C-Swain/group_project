627bf2693bbdff67602319f424e15e35bba2ad3e
Salim Mustafa <mustafasalim000@gmail.com>
2021-10-16 20:49:15 +0000

    created dummy data for sql

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,8 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password) 
+VALUES 
+  ('Alice', 'alice@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+  ('Kira', 'kira@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+  ('Salim', 'salim@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+  ('Anju', 'anju@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+  ('Catherine', 'catherine@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
---
+++ db/seeds/06_products.sql
@@ -0,0 +1,5 @@
+INSERT INTO products (item_id, name, description, link)
+VALUES
+  (1, 'product1','product1','product1'),
+  (5, 'product5','product5','product5'),
+  (3, 'product3', 'product3','product3');
