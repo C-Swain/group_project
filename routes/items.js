11b4f9e6b5c5786f5761bc11d3826362058f1222
anju-kumaran <anju.naduth@gmail.com>
2021-10-21 10:38:05 -0400

    Added the functionalities to post a new product

--- routes/items.js
+++ routes/items.js
@@ -8,9 +8,9 @@
 const express = require('express');
 const router  = express.Router();
 
-const { getProductsByCategoryName, deleteProduct, filterByPrice
+const { getProductsByCategoryName, filterByPrice, addProduct, deleteProduct
+} = require('../database');
 
-} = require('../database')
 
 
 // code that diplays api of products for trouble shooting
@@ -29,6 +29,48 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+// To go to add-items page
+router.get("/new", (req, res) => {
+  const templateVars = req.params;
+  console.log('temp vars: ',templateVars);
+  res.render("add-items", templateVars);
+});
+
+// To add a new product to the database
+router.post("/new", (req, res) => {
+  const name = req.body.itemName;
+  const description = req.body.itemDescription;
+  const category_id = req.body.itemCategory;
+  const price = req.body.itemPrice;
+  const image_url = req.body.itemImage;
+  const seller_id = req.session.user_id;
+  let is_featured = false;
+
+  if(req.body.itemFeatured) {
+    is_featured = req.body.itemFeatured;
+  }
+
+  let product = {
+    name,
+    description,
+    category_id,
+    price,
+    is_featured,
+    image_url,
+    seller_id
+  }
+
+  console.log("product: ", product);
+
+  addProduct(product, db)
+  .then (data => {
+     console.log('id here:',data.id);
+
+    res.redirect("/api/users/store");
+  });
+
+});
 
 router.get("/filterPrice", (req, res) => {
   const templateVars = req.params;
