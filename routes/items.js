26870985e1cfb7997a81634987e701e55602e289
salimM-code <mustafasalim000@gmail.com>
2021-10-20 21:15:37 -0400

    removed a console.log

--- routes/items.js
+++ routes/items.js
@@ -70,7 +70,7 @@ router.post("/filterPrice", (req, res) => {
 // we will have a function that  makes a sql query based on the category inputed in order to select only thoose items
 getProductsByCategoryName(category, db)
 .then(data => {
-console.log(data)
+// console.log(data)
  const templateVars = {data ,category , user};
 
 
@@ -93,7 +93,13 @@ router.post("/:prodId/delete", (req, res) => {
 })
 
 
-
+ // message route
+ router.get("/messages", (req, res) => {
+  getAllTexts(db)
+  .then(data => {
+    res.json(data)
+  })
+})
 
 return router
 
